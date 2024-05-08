677d75d2a3d6bcc3d0a7098c3e1a119d0d0fe96d
Sergei Malyshka <104466946+SergMalyshka@users.noreply.github.com>
2024-05-08 17:34:45 -0400

    original structure

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const {Schema, model} = require('mongoose')
+
+var validateEmail = function(email) {
+    var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email)
+};
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, "Please enter a valid email address"]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model ('User', UserSchema)
+
+module.exports = User;
